a1e6fdd171ccc83c8e996a22ef6f0c1021897eb5
tekkub :bear:  <tekkub@gmail.com>
2011-07-17 00:26:17 -0600

    New southbound table too!

--- main.js
+++ main.js
@@ -10,49 +10,41 @@ $(function() {
     }
   }
 
-  recheck_trains = function() {
-    var now = new Date()
-    var now_time = now.getHours()*100 + now.getMinutes()
-    var past_midnight = now_time < 400
 
-    // gt(0) to skip the header row
-    $("#southbound tr:gt(0)").each(function() {
-      // eq(1) checks the SECOND column, so we don't miss a train that's between 20th and 16th
-      var this_time = Number($(this).find("td:eq(1)").text())
-      if (this_time < now_time || (past_midnight && this_time >= 400)) {
-        // teklog("Hiding SB " + this_time, this_time < now_time, past_midnight, this_time >= 400)
-        $(this).hide()
+  function buildTimetable(schedule, timeFunc) {
+    var now = new Date()
+    var in24hr = new Date(now.getTime() + 24 * 60 * 60 * 1000)
+    var year = now.getFullYear()
+    var month = now.getMonth()
+    var day = now.getDate()
+
+    var timetable = []
+
+    $.each(schedule, function(i,v) {
+      var hour = v[0] / 100
+      var min = v[0] % 100
+      var baseTime = new Date(year, month, day, hour, min, 0, 0)
+
+      for (t = 0; t <= v[1]; t++) {
+        var thisTime = new Date(baseTime.getTime() + t * v[2] * 60 * 1000)
+        if (thisTime >= now && thisTime < in24hr) timetable.push(timeFunc(thisTime));
       }
     })
-    $("#southbound tr:visible:gt(3)").hide()
 
-    var this_day = now.getDay()
-    var is_weekend = (this_day == 6 || this_day == 0 || this_day == 1 && past_midnight)
-    // teklog("Weekend: "+ is_weekend)
-    if (!is_weekend) $(".bonus").hide();
+    $.each(schedule, function(i,v) {
+      var hour = v[0] / 100
+      var min = v[0] % 100
+      var todayTime = new Date(year, month, day, hour, min, 0, 0)
+      var baseTime = new Date(todayTime.getTime() + 24 * 60 * 60 * 1000)
 
-    // Never have an empty table
-    if ($("#southbound tr:visible").size() < 2) {
-      // teklog("Don't want empty table")
-      $("#southbound tr:lt(4)").show()
-    }
+      for (t = 0; t <= v[1]; t++) {
+        var thisTime = new Date(baseTime.getTime() + t * v[2] * 60 * 1000)
+        if (thisTime >= now && thisTime < in24hr) timetable.push(timeFunc(thisTime));
+      }
+    })
 
-    if ($("#southbound tr.bonus:visible").size() == 0) $(".bonus").hide();
+    return timetable
   }
-
-  recheck_trains()
-  setInterval("recheck_trains()",'10000')
-
-
-  // Table toggle
-  $("#north").click(function() {
-    $("#north, #northbound").hide()
-    $("#south, #southbound").show()
-  })
-  $("#south").click(function() {
-    $("#north, #northbound").show()
-    $("#south, #southbound").hide()
-  })
 
 
   // Northbound
@@ -69,42 +61,40 @@ $(function() {
       "eightcali": eight,
     }
   }
-  var schedule = [
+  var northSchedule = [
     [  23,  1, 30], // 00:23 - 00:53 every 30
     [ 555, 60, 15], // 05:00 - 20:55 every 15
     [2123,  5, 30], // 21:23 - 23:53 every 30
   ]
-  var northTimetable = []
-  var now = new Date()
-  var in24hr = new Date(now.getTime() + 24 * 60 * 60 * 1000)
-  var year = now.getFullYear()
-  var month = now.getMonth()
-  var day = now.getDate()
+  var northTimetable = buildTimetable(northSchedule, getNorthboundTimes)
 
-  $.each(schedule, function(i,v) {
-    var hour = v[0] / 100
-    var min = v[0] % 100
-    var baseTime = new Date(year, month, day, hour, min, 0, 0)
 
-    for (t = 0; t <= v[1]; t++) {
-      var thisTime = new Date(baseTime.getTime() + t * v[2] * 60 * 1000)
-      if (thisTime >= now && thisTime < in24hr) northTimetable.push(getNorthboundTimes(thisTime));
+  // Southbound
+  // 20th & welton = 16 - 4min
+  // 18th & stout = 16th - 1min
+  // Theatre = 16th + 2min
+  // oxford = 16th + 20min
+  function getSouthboundTimes(baseTime) {
+    var twenty = new Date(baseTime.getTime() - 4 * 60 * 1000)
+    var eight  = new Date(baseTime.getTime() - 1 * 60 * 1000)
+    var oxford = new Date(baseTime.getTime() + 20 * 60 * 1000)
+    return {
+      "oxford": oxford,
+      "sixstout": baseTime,
+      "twentywelton": twenty,
     }
-  })
+  }
+  var southSchedule = [
+    [  16,  3, 30], // 00:16 - 01:46 every 30
+    [ 525, 66, 15], // 05:25 - 21:55 every 15
+    [2216,  3, 30], // 22:16 - 23:46 every 30
+  ]
+  var southTimetable = buildTimetable(southSchedule, getSouthboundTimes)
 
-  $.each(schedule, function(i,v) {
-    var hour = v[0] / 100
-    var min = v[0] % 100
-    var todayTime = new Date(year, month, day, hour, min, 0, 0)
-    var baseTime = new Date(todayTime.getTime() + 24 * 60 * 60 * 1000)
 
-    for (t = 0; t <= v[1]; t++) {
-      var thisTime = new Date(baseTime.getTime() + t * v[2] * 60 * 1000)
-      if (thisTime >= now && thisTime < in24hr) northTimetable.push(getNorthboundTimes(thisTime));
-    }
-  })
-
+  // And the good stuff
   var nb = Tempo.prepare("northbound")
+  var sb = Tempo.prepare("southbound")
   rebuild_timetables = function() {
     var now = new Date()
     if ((now.getTime() - last_loaded.getTime()) >= 60 * 60 * 1000) {
@@ -113,9 +103,21 @@ $(function() {
     }
 
     teklog("Rebuilding timetable", now)
-    $("#northbound tr:gt(0)").remove()
+    $("#northbound tr:gt(0), #southbound tr:gt(0)").remove()
     nb.render(northTimetable)
+    sb.render(southTimetable)
   }
   rebuild_timetables()
   setInterval("rebuild_timetables()",'10000')
+
+
+  // Table toggle
+  $("#north").click(function() {
+    $("#north, #northbound").hide()
+    $("#south, #southbound").show()
+  })
+  $("#south").click(function() {
+    $("#north, #northbound").show()
+    $("#south, #southbound").hide()
+  })
 })
